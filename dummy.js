f33e5d584cc53d0c4def55184d3090e705bc8b98
Kevin Kim <kdkim7676@gmail.com>
2022-02-05 03:06:15 -0800

    initial backend endpoint created

---
+++ dummy.js
@@ -0,0 +1,32 @@
+module.exports = {
+    1 : {
+        teamName: 'Manchester',
+        coach: 'a',
+        uniform: 'b',
+        stadium: 'c'
+    },
+    2 : {
+        teamName: 'Liverpool',
+        coach: 'd',
+        uniform: 'e',
+        stadium: 'f'
+    },
+    3 : {
+        teamName: 'Chelsea',
+        coach: 'g',
+        uniform: 'h',
+        stadium: 'i'
+    },
+    4 : {
+        teamName: 'Manchester',
+        coach: 'j',
+        uniform: 'k',
+        stadium: 'l'
+    },
+    5 : {
+        teamName: 'Liverpool',
+        coach: 'm',
+        uniform: 'n',
+        stadium: 'o'
+    }
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const dummyData = require('./dummy')
+const cors = require("cors")
+
+const app = express()
+
+const HOST = 'http://localhost'
+const PORT = 8888
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', cors(), async (req,res) => {
+    res.send('rickshouse soccer api')
+})
+
+app.get('/team', cors(), async (req,res) => {
+    res.status(200).json(dummyData)
+})
+
+app.get('/home', cors(), async (req, res) => {
+    res.send("data for home")
+})
+
+app.post('/post_team', async (req, res) => {
+    let { teamData } = req.body
+    console.log(teamData)
+})
+
+
+app.listen(PORT, () => {
+    console.log(`API running at ${HOST}:${PORT}!`)
+})
