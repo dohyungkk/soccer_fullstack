bb9c4166e4bc5089b1ed7b97f0d63f8060bf1f46
Kevin Kim <kdkim7676@gmail.com>
2022-03-06 13:03:33 -0800

    minor changes on edit and delete

--- controller/controller.js
+++ controller/controller.js
@@ -1,5 +1,4 @@
 import fs from "fs"
-import { v4 as uuid } from 'uuid';
 
 let teams = []
 
@@ -11,18 +10,14 @@ export const getTeams = (req, res) => {
 
 export const createTeam = async (req, res) => {
     let { teamData } = req.body
-    teams.push({...teamData, id: uuid()})
-    console.log(teams)
+    teams.push({...teamData})
+    // console.log(teams)
     let addData = JSON.stringify(teamData)
     fs.writeFile("data.json", addData, (err) => {
         if (err) throw err
         console.log("new data added")
     })
 }
-
-// export const getTeam = (req, res) => {
-//     res.send(req.params.id)
-// }
 
 export const updateTeam = (req, res) => {
     const team = teams.find((team) => team.id === req.params.id)
@@ -35,7 +30,6 @@ export const updateTeam = (req, res) => {
 }
 
 export const deleteTeam = (req, res) => {
-    console.log(req.params.id, 'has been deleted')
+    console.log('data has been deleted')
     teams = teams.filter((team) => team.id === req.params.id)
-    
 }
