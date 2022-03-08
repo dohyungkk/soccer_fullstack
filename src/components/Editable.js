4477a3682d11bc953139f2709a1e5ef236d0fa09
Kevin Kim <32403383+dohyungkk@users.noreply.github.com>
2022-03-07 16:38:29 -0800

    Revert "Owen"

--- src/components/Editable.js
+++ src/components/Editable.js
@@ -5,9 +5,6 @@ import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
 
 const Editable = ({
-
-    handleEditTableSubmit,
-
     editTableData,
     editData,
     handleCancelClick,
@@ -58,11 +55,7 @@ const Editable = ({
                 />
             </TableCell>
             <TableCell>
-
-                <Button type="submit" onClick={handleEditTableSubmit} variant="contained">Save</Button>
-
                 <Button type="submit" variant="contained">Save</Button>
-
                 <Button type="button" onClick={handleCancelClick} variant="contained" color="error">Cancel</Button>
             </TableCell>
         </TableRow>
